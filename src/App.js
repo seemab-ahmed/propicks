d3aef75e50d01853834da75111b1477cb133447a
Seemab Ahmed <seemab40615@gmail.com>
2024-12-26 01:29:56 -0800

    MobileResponsive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import ScrollSmoother from "./assets/js/ScrollSmoother.min.js";
+import ScrollSmoother from "./assets/js/ScrollSmoother.min.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
